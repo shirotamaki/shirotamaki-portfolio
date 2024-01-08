998a67d2368539f2fc2255d6d93e99ecce8a9a8d
shirotamaki <shiro.tamaki63@gmail.com>
2024-01-08 22:42:59 +0900

    ESLint, Prettier を設定する

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,5 @@
+import { defineNuxtConfig } from '@nuxt/bridge'
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
+  devtools: { enabled: true },
 })
