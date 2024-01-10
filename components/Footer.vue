7f8feca968816e2a7ff8c0c3e63f30341ae958e8
shirotamaki <shiro.tamaki63@gmail.com>
2024-01-10 20:28:22 +0900

    Header, Footer を修正する

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,5 +1,5 @@
 <template>
-  <footer class="grid justify-items-center mb-8">
+  <footer class="grid justify-items-center mt-16 mb-8">
     <small class="font-NotoSansJP text-lg">© 2023 shirotamaki</small>
   </footer>
 </template>
