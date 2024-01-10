ad6e5415232fb65bb67f75145ef82d00c186e467
shirotamaki <shiro.tamaki63@gmail.com>
2024-01-10 11:58:53 +0900

    土台になるファイルに仮のコードを記述

---
+++ pages/index.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <section>
+      <img src="~/assets/images/myphoto.png" alt="My Photo" />
+      <p><a href="https://twitter.com/tamaki460" target="_blank" rel="noopener noreferrer">X</a></p>
+      <p><a href="https://github.com/shirotamaki" target="_blank" rel="noopener noreferrer">github</a></p>
+      <p><a href="https://shirotamaki.hatenablog.com/" target="_blank" rel="noopener noreferrer">blog</a></p>
+      <p><a href="https://booklog.jp/users/tamaki460/all?category_id=3530577&status=3&rank=all&sort=sort_desc&display=front" target="_blank" rel="noopener noreferrer">ブクログ</a></p>
+    </section>
+
+    <ul>
+      <li>
+        <NuxtLink to="/portfolio">[自作サービス]アウトドア般若心経</NuxtLink>
+      </li>
+      <li>
+        <NuxtLink to="/lt">外部登壇</NuxtLink>
+      </li>
+      <li>
+        <NuxtLink to="/photo"></NuxtLink>
+      </li>
+    </ul>
+  </div>
+</template>
