6ff316a03de88fe0172fea0cd415ec2712dbe382
shirotamaki <shiro.tamaki63@gmail.com>
2024-01-10 23:06:09 +0900

    work（作品）のコンポーネントを修正する

---
+++ pages/work.vue
@@ -0,0 +1,99 @@
+<template>
+  <div class="grid justify-items-center font-NotoSansJP">
+    <article class="sm:w-96 w-72">
+      <h1>
+        <img
+          src="~/assets/images/outdoor_heart_sutra_1200_630.png"
+          alt="outdoor_heart_sutra"
+          class="sm:w-96 w-72"
+        />
+      </h1>
+      <section class="mb-16">
+        <section class="mb-8">
+          <h2 class="text-xl mb-4 border-b-2">サービス概要</h2>
+          <p class="leading-7">
+            「アウトドア般若心経」というサービスは、街の看板等から集めた般若心経 278
+            文字の写真の管理が面倒な問題を解決することができる、アウトドア般若心経ユーザー向けの写真管理アプリです。
+          </p>
+          <p class="leading-7">
+            ユーザーは、般若心経に含まれる 278
+            文字を一文字ずつ写真に収め管理することができ、文字の収集状況を経文の並び順で視覚的に確認することができます。また撮影場所の位置情報、メモを記録する機能も備えています。
+          </p>
+          <p class="leading-7">Google アカウントがあれば無料でご利用いただけます。</p>
+        </section>
+        <section class="mb-8">
+          <h3 class="text-lg">サービス URL</h3>
+          <div class="underline hover:opacity-75 transition-all duration-100 text-sm">
+            <a href="https://www.outdoor-heart-sutra.com/" target="_blank" rel="noopener noreferrer"
+              >https://www.outdoor-heart-sutra.com</a
+            >
+          </div>
+        </section>
+        <section>
+          <h3 class="text-lg">GitHub リポジトリ</h3>
+          <div class="underline hover:opacity-75 transition-all duration-100 text-sm">
+            <a
+              href="https://github.com/shirotamaki/outdoor-heart-sutra"
+              target="_blank"
+              rel="noopener noreferrer"
+              >https://github.com/shirotamaki/outdoor-heart-sutra</a
+            >
+          </div>
+        </section>
+      </section>
+      <section class="mb-16">
+        <section class="mb-8">
+          <h2 class="text-xl mb-4 border-b-2">サービス紹介ブログ</h2>
+          <h3 class="text-lg mb-2">リリースブログ</h3>
+          <p class="leading-7">
+            Web
+            アプリの概要や使い方、技術スタックやこだわった点、また開発過程での苦労や学んだこと、さらには今後の展望まで詳しく書いています。
+          </p>
+          <div class="underline hover:opacity-75 transition-all duration-100 text-sm">
+            <a
+              href="https://shirotamaki.hatenablog.com/entry/release-blog"
+              target="_blank"
+              rel="noopener noreferrer"
+              >https://shirotamaki.hatenablog.com/entry/release-blog</a
+            >
+          </div>
+        </section>
+        <section>
+          <h3 class="text-lg mb-2">旅ログ</h3>
+          <p class="leading-7">ドッグフーディングの様子を著した旅の記録になります。</p>
+          <div class="underline hover:opacity-75 transition-all duration-100 text-sm">
+            <a
+              href="https://shirotamaki.hatenablog.com/entry/travel-log-first"
+              target="_blank"
+              rel="noopener noreferrer"
+              >https://shirotamaki.hatenablog.com/entry/travel-log-first</a
+            >
+          </div>
+          <div class="underline hover:opacity-75 transition-all duration-100 text-sm">
+            <a
+              href="https://shirotamaki.hatenablog.com/entry/travel-log-second"
+              target="_blank"
+              rel="noopener noreferrer"
+              class="block"
+              >https://shirotamaki.hatenablog.com/entry/travel-log-second</a
+            >
+          </div>
+        </section>
+      </section>
+
+      <section class="mb-8">
+        <h2 class="text-xl mb-4 border-b-2">反響</h2>
+        <P>反響結果をまとめたブログです</P>
+        <div class="underline hover:opacity-75 transition-all duration-100 text-sm">
+          <a
+            href="https://shirotamaki.hatenablog.com/entry/2023/12/15/214224"
+            target="_blank"
+            rel="noopener noreferrer"
+            class="block"
+            >https://shirotamaki.hatenablog.com/entry/2023/12/15/214224</a
+          >
+        </div>
+      </section>
+    </article>
+  </div>
+</template>
