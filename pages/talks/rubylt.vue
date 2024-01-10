6845995bbfebe795a8750b52141a68d6d966b42f
shirotamaki <shiro.tamaki63@gmail.com>
2024-01-10 23:06:51 +0900

    rubyltのページを作成する

---
+++ pages/talks/rubylt.vue
@@ -0,0 +1,66 @@
+<template>
+  <div class="grid justify-items-center font-NotoSansJP">
+    <article class="sm:w-96 w-72">
+      <h1>
+        <img src="~/assets/images/ruby_lt.jpeg" alt="ruby lt" class="sm:w-96 w-72" />
+      </h1>
+      <section class="">
+        <section class="mb-8">
+          <h2 class="text-xl mt-8 mb-4 border-b-2">概要</h2>
+          <p class="leading-7">フィヨルドブートキャンプ内でLT会を主催しました。</p>
+          <p class="leading-7 mb-4">開催準備から司会、進行役も務めました。</p>
+          <iframe
+            class="speakerdeck-iframe"
+            frameborder="0"
+            src="https://speakerdeck.com/player/165b6943ac624b81bba3c5f7f9f45cb8"
+            title="fjordbootcamp_20210424_mc_slide"
+            allowfullscreen="true"
+            style="
+              border: 0px;
+              background: padding-box padding-box rgba(0, 0, 0, 0.1);
+              margin: 0px;
+              padding: 0px;
+              border-radius: 6px;
+              box-shadow: rgba(0, 0, 0, 0.2) 0px 5px 40px;
+              width: 100%;
+              height: auto;
+              aspect-ratio: 560 / 315;
+            "
+            data-ratio="1.7777777777777777"
+          ></iframe>
+          <p class="mt-4">日時: 2021年4月24日（土）</p>
+          <div>
+            <span>場所: </span>
+            <span class="underline hover:opacity-75 transition-all duration-100 text-sm">
+              <a href="https://bootcamp.fjord.jp/" target="_blank" rel="noopener noreferrer"
+                >フィヨルドブートキャンプ（オンライン）</a
+              >
+            </span>
+          </div>
+        </section>
+        <section class="">
+          <h2 class="text-xl mb-4 border-b-2">登壇資料</h2>
+          <iframe
+            class="speakerdeck-iframe"
+            frameborder="0"
+            src="https://speakerdeck.com/player/d747b43a06174c0fa3e0f587b3c5da13"
+            title="fjordbootcamp_20210424"
+            allowfullscreen="true"
+            style="
+              border: 0px;
+              background: padding-box padding-box rgba(0, 0, 0, 0.1);
+              margin: 0px;
+              padding: 0px;
+              border-radius: 6px;
+              box-shadow: rgba(0, 0, 0, 0.2) 0px 5px 40px;
+              width: 100%;
+              height: auto;
+              aspect-ratio: 560 / 315;
+            "
+            data-ratio="1.7777777777777777"
+          ></iframe>
+        </section>
+      </section>
+    </article>
+  </div>
+</template>
