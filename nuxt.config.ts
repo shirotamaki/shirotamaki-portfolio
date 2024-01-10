1c3e60de49f5501f9218cee216963da6a28a4d65
shirotamaki <shiro.tamaki63@gmail.com>
2024-01-10 14:30:15 +0900

    FontAwesomeをセットアップする

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,4 +2,6 @@ import { defineNuxtConfig } from '@nuxt/bridge'
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
+  css: ['@fortawesome/fontawesome-svg-core/styles.css'],
+  plugins: ['@/plugins/fontawesome.ts'],
 })
