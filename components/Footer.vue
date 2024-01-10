5de0dd649d1f46f56c7e7c93c7241c881486f1df
shirotamaki <shiro.tamaki63@gmail.com>
2024-01-10 11:57:08 +0900

    header, footer をコンポーネント化

---
+++ components/Footer.vue
@@ -0,0 +1,5 @@
+<template>
+  <footer>
+    <small className='md:text-base text-xs'>© 2023 shirotamaki</small>
+  </footer>
+</template>
---
+++ components/Header.vue
@@ -0,0 +1,8 @@
+<template>
+  <header>
+    <NuxtLink to="/">
+    <h1>Shiro Tamaki</h1>
+    </NuxtLink>
+    <h2>Portfolio</h2>
+  </header>
+</template>
