7136dffb633eaebab7f801afe3211f49598e6f09
shirotamaki <shiro.tamaki63@gmail.com>
2024-01-11 11:28:46 +0900

    work（作品）にbootcampを追加する

--- components/Work.vue
+++ components/Work.vue
@@ -1,12 +1,21 @@
 <template>
   <div class="grid justify-items-center font-Poppins">
     <ul>
-      <li>
-        <NuxtLink to="/work">
+      <li class="mb-8">
+        <NuxtLink to="/works/outdoor-heart-sutra">
           <img
             src="~/assets/images/outdoor_heart_sutra_1200_630.png"
             alt="outdoor_heart_sutra"
-            class="sm:w-96 w-72 hover:opacity-50 transition-all duration-100 shadow-md"
+            class="sm:w-96 w-72 hover:opacity-55 transition-all duration-100 shadow-md"
+          />
+        </NuxtLink>
+      </li>
+      <li>
+        <NuxtLink to="/works/bootcamp">
+          <img
+            src="~/assets/images/bootcamp.png"
+            alt="bootcamp"
+            class="sm:w-96 w-72 hover:opacity-75 transition-all duration-100 shadow-xl"
           />
         </NuxtLink>
       </li>
---
+++ pages/works/bootcamp.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="grid justify-items-center font-NotoSansJP">
+    <article class="sm:w-96 w-72">
+      <h1>
+        <img src="~/assets/images/bootcamp.png" alt="bootcamp" class="sm:w-96 w-72" />
+      </h1>
+      <section class="mt-8 mb-16">
+        <section class="mb-4">
+          <h2 class="text-xl mb-4 border-b-2">概要</h2>
+          <p class="leading-7">
+            プログラミングスクール「フィヨルドブートキャンプ」では、実際の仕事を想定したチーム開発のカリキュラムが提供されています。
+          </p>
+          <p class="leading-7">
+            以下Webサービスの開発チームに参加し、開発プログジェクトに携わりました。
+          </p>
+        </section>
+        <section class="mb-8">
+          <h3 class="text-lg">GitHub リポジトリ</h3>
+          <div class="underline hover:opacity-75 transition-all duration-100 text-sm break-words">
+            <a href="https://github.com/fjordllc/bootcamp" target="_blank" rel="noopener noreferrer"
+              >https://github.com/fjordllc/bootcamp</a
+            >
+          </div>
+        </section>
+        <section>
+          <ul>
+            <li class="mt-4 ml-4 list-disc">参加期間: 4ヶ月</li>
+            <li class="mt-1 ml-4 list-disc">対応したイシュー: 11</li>
+            <li class="mt-1 ml-4 list-disc">レビューしたプルリクエスト: 13</li>
+          </ul>
+        </section>
+      </section>
+      <section class="mb-16">
+        <section class="mb-8">
+          <h2 class="text-xl mb-4 border-b-2">チーム開発で行ったこと</h2>
+          <p class="leading-7">
+            チーム開発で対応したイシューとレビューしたプルリクエストの一覧をまとめたブログになります。
+          </p>
+          <div class="underline hover:opacity-75 transition-all duration-100 text-sm break-words">
+            <a
+              href="https://shirotamaki.hatenablog.com/entry/2023/01/19/001713"
+              target="_blank"
+              rel="noopener noreferrer"
+              >https://shirotamaki.hatenablog.com/entry/2023/01/19/001713</a
+            >
+          </div>
+        </section>
+      </section>
+    </article>
+  </div>
+</template>
