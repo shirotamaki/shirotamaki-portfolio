6ec7665a56a788b90303bfaca8049ee2060974dd
shirotamaki <shiro.tamaki63@gmail.com>
2024-01-11 17:14:04 +0900

    About ページを作成する

---
+++ components/About.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="grid justify-items-center">
+    <NuxtLink to="/about">
+      <img
+        src="~/assets/images/business_card_front.png"
+        alt="business_card"
+        class="sm:w-96 w-80 hover:opacity-75 transition-all duration-100 shadow-xl"
+      />
+    </NuxtLink>
+  </div>
+</template>
---
+++ pages/about.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="grid justify-items-center">
+    <div class="mb-4">
+      <img
+        src="~/assets/images/business_card_profile.png"
+        alt="business_card_profile"
+        class="sm:w-[480px] w-80 shadow-lg"
+      />
+    </div>
+    <div class="mb-4">
+      <img
+        src="~/assets/images/business_card_programming.png"
+        alt="business_card_programming"
+        class="sm:w-[480px] w-80 shadow-lg"
+      />
+    </div>
+    <div class="mb-4">
+      <img
+        src="~/assets/images/business_card_career.png"
+        alt="business_card_career"
+        class="sm:w-[480px] w-80 shadow-lg"
+      />
+    </div>
+    <div class="mb-4">
+      <img
+        src="~/assets/images/business_card_skills.png"
+        alt="business_card_skills"
+        class="sm:w-[480px] w-80 shadow-lg"
+      />
+    </div>
+  </div>
+</template>
