457bce1caadd55b858abc3059c7f13057fe8b55f
shirotamaki <shiro.tamaki63@gmail.com>
2024-01-11 17:12:32 +0900

    Headerのテキストサイズを修正

--- components/Header.vue
+++ components/Header.vue
@@ -2,9 +2,11 @@
   <header>
     <div class="grid justify-items-center my-16 font-Poppins">
       <NuxtLink to="/">
-        <h1 class="text-5xl hover:opacity-50 transition-all duration-100 mb-4">Shiro Tamaki</h1>
+        <h1 class="sm:text-6xl text-5xl hover:opacity-50 transition-all duration-100 mb-4">
+          Shiro Tamaki
+        </h1>
       </NuxtLink>
-      <h2 class="text-3xl">Portfolio</h2>
+      <h2 class="sm:text-4xl text-3xl">Portfolio</h2>
     </div>
   </header>
 </template>
