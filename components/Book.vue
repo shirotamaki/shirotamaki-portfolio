734b4b0643170f639f664413893f3166f6c6afc6
shirotamaki <shiro.tamaki63@gmail.com>
2024-01-29 12:12:00 +0900

    Bookコンポーネントを作成

---
+++ components/Book.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="grid justify-items-center font-Poppins">
+    <ul>
+      <li class="mb-8">
+        <NuxtLink to="/book">
+          <img
+            src="~/assets/images/book.png"
+            alt="Book List"
+            class="sm:w-96 w-80 hover:opacity-75 transition-all duration-100 shadow-md"
+          />
+        </NuxtLink>
+      </li>
+    </ul>
+  </div>
+</template>
